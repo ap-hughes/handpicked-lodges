cba4d1eea59c64cea035fb0f9f67938fd43f9415
Alex Hughes <aphughes@talk21.com>
2018-07-19 12:15:05 +0100

    Created basic page structure

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,24 @@
 class PagesController < ApplicationController
   def home
   end
+  def about_the_area
+  end
+  def contact
+  end
+  def own_a_property
+  end
+  def go_catered
+  end
+  def getting_here
+  end
+  def pet_friendly
+  end
+  def families
+  end
+  def about_us
+  end
+  def terms_and_conditions
+  end
+  def frequently_asked_questions
+  end
 end
