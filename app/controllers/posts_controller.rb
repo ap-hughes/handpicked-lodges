ff1d1285af3b8cbecd2384252668eec24a7898c9
Alex Hughes <aphughes@talk21.com>
2018-10-15 11:49:53 +0100

    Cleaned up posts controller - removed admin only actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,60 +1,11 @@
 class PostsController < ApplicationController
-  before_action :authenticate_admin!, except: [:index, :show]
-  before_action :find_post, only: [:edit, :update, :show, :destroy]
-
   def index
     @posts = Post.order(created_at: :desc)
   end
 
   def show
+    @post = Post.find(params[:id])
     @previous_post = @post.next
     @next_post = @post.previous
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-    if @post.save
-      flash[:notice] = "Successfully created post!"
-      redirect_to post_path(@post)
-    else
-      flash[:alert] = "Error creating new post!"
-      render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @post.update_attributes(post_params)
-      flash[:notice] = "Successfully updated post"
-      redirect_to post_path(@post)
-    else
-      flash[:alert] = "Error updating post"
-      render :edit
-    end
-  end
-
-  def destroy
-    if @post.destroy
-      flash[:notice] = "Successfully deleted post!"
-      redirect_to posts_path
-    else
-      flash[:alert] = "Error deleting post!"
-    end
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :body, :photo)
-  end
-
-  def find_post
-    @post = Post.find(params[:id])
   end
 end
