3ebb17515bbee34044b2fbfb5011a909860b4268
Alex Hughes <aphughes@talk21.com>
2019-07-18 22:10:44 +0100

    No longer need the validation as now using ActiveStorage for the main pic. This was causing an error as this photo was not being added on Post create, wasn't possible to with form.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,6 @@ class Post < ApplicationRecord
   #title maximum 120 characters
   validates :title, presence: true, length: {maximum: 120}
   validates :body, presence: true
-  validates :photo, presence: true, on: :create
   validates :image, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg']
   before_destroy :purge_active_storage
 
